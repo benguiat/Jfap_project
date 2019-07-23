5f2f5f65abbd8a706eea767230ce58ab1f10ea45
Katherine Dunfield <katherine.dunfield@dfki.de>
2019-07-23 15:18:00 +0200

    JsonMarshallingContext WIP

--- src/main/java/de/unisaar/faphack/model/JsonMarshallingContext.java
+++ src/main/java/de/unisaar/faphack/model/JsonMarshallingContext.java
@@ -32,26 +32,64 @@ public class JsonMarshallingContext implements MarshallingContext {
 
   @Override
   public void save(Storable s) {
-    JSONObject obj = new JSONObject();
-    
-    obj.put("game", s);
-    
-    try (FileWriter file = new FileWriter("employees.json")) {
- 
-            file.write(obj.toJSONString());
-            file.flush();
- 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    
+//    JSONObject obj = new JSONObject();
+//    
+//    
+//    
+//    obj.put("game", s.toString());
+//    
+//    
+//    try (FileWriter f = new FileWriter(file)) {
+// 
+//            f.write(obj.toJSONString());
+//            f.flush();
+// 
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    
     
   }
 
-  public Storable read() {
-    // TODO Auto-generated method stub
-    return null;
-  }
+  @Override
+    public Storable read() {
+
+    JSONParser parser = new JSONParser();
+    
+    Storable request = null;
+
+        try (Reader reader = new FileReader(file)) {
+
+            JSONObject obj = (JSONObject) parser.parse(reader);
+            
+            
+            System.out.println(obj);
+            
+            
+
+//            String name = (String) jsonObject.get("name");
+//            System.out.println(name);
+//
+//            long age = (Long) jsonObject.get("age");
+//            System.out.println(age);
+//
+//            // loop array
+//            JSONArray msg = (JSONArray) jsonObject.get("messages");
+//            Iterator<String> iterator = msg.iterator();
+//            while (iterator.hasNext()) {
+//                System.out.println(iterator.next());
+//            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+      return request;
+
+    }
+
+    
 
   @Override
   public void write(String key, Storable object) {
