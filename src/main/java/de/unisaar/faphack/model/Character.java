3907ded14402a560baf7121db283540b622cbaba
Lena Meth <eleni.metheniti@dfki.de>
2019-07-23 13:51:37 +0200

    I can't believe it's not butter

--- src/main/java/de/unisaar/faphack/model/Character.java
+++ src/main/java/de/unisaar/faphack/model/Character.java
@@ -18,154 +18,154 @@ import de.unisaar.faphack.model.map.Tile;
  */
 public class Character implements Storable, TraitedTileOccupier {
 
-  /**
-   * I'm currently on this level
-   */
-  private int level = 0;
+    /**
+     * I'm currently on this level
+     */
+    private int level = 0;
 
-  /**
-   * The position of the character.
-   */
-  protected Tile tile;
-  /**
-   * The characters inventory. The amount of items in the inventory is limited by
-   * the maxWeight value of a character.
-   */
-  protected List<Wearable> items = new ArrayList<>();
+    /**
+     * The position of the character.
+     */
+    protected Tile tile;
+    /**
+     * The characters inventory. The amount of items in the inventory is limited
+     * by the maxWeight value of a character.
+     */
+    protected List<Wearable> items = new ArrayList<>();
 
-  /**
-   * The base health of the character, which can be modified by Modifiers.
-   *
-   * If health is zero, this character is dead!
-   */
-  int health = 100;
+    /**
+     * The base health of the character, which can be modified by Modifiers.
+     *
+     * If health is zero, this character is dead!
+     */
+    int health = 100;
 
-  /**
-   * The base magic of the character, which can be modified by Modifiers.
-   */
-  int magic = 0;
+    /**
+     * The base magic of the character, which can be modified by Modifiers.
+     */
+    int magic = 0;
 
-  /**
-   * The base power of the character, which can be modified by Modifiers.
-   */
-  int power = 0;
+    /**
+     * The base power of the character, which can be modified by Modifiers.
+     */
+    int power = 0;
 
-  /**
-   * This models the character's trait, i.e., how effective are the different
-   * skills of the character.
-   */
-  protected MultiplicativeEffect skills;
+    /**
+     * This models the character's trait, i.e., how effective are the different
+     * skills of the character.
+     */
+    protected MultiplicativeEffect skills;
 
-  /**
-   * This might be shield / bodyarmor / etc.
-   */
-  protected List<Wearable> armor = new ArrayList<>();
+    /**
+     * This might be shield / bodyarmor / etc.
+     */
+    protected List<Wearable> armor = new ArrayList<>();
 
-  /**
-   * The maximal amount of weight the character can carry. The sum of the weight
-   * of all items in the character's inventory plus the armor must not exceed this
-   * value.
-   */
-  protected int maxWeight;
+    /**
+     * The maximal amount of weight the character can carry. The sum of the
+     * weight of all items in the character's inventory plus the armor must not
+     * exceed this value.
+     */
+    protected int maxWeight;
 
-  /**
-   * All effects that currently apply on the character, for example damage or heal
-   * over time
-   */
-  protected Set<CharacterModifier> activeEffects = new HashSet<>();
+    /**
+     * All effects that currently apply on the character, for example damage or
+     * heal over time
+     */
+    protected Set<CharacterModifier> activeEffects = new HashSet<>();
 
-  /**
-   * That's my name
-   */
-  protected String name;
+    /**
+     * That's my name
+     */
+    protected String name;
 
-  /**
-   * That's my role
-   */
-  protected String role;
+    /**
+     * That's my role
+     */
+    protected String role;
 
-  /**
-   * The currently active weapon
-   */
-  protected Wearable activeWeapon;
+    /**
+     * The currently active weapon
+     */
+    protected Wearable activeWeapon;
 
-  public Character() {
-
-  }
-
-  /**
-   * Change my position to the given Tile.
-   *
-   * @param destination
-   * @return void
-   */
-  public void move(Tile destination) {
-    // TODO: FILL THIS: KTN, check it!
-
-      this.tile = destination;
-  }
-
-  /**
-   * Pick up the given Wearable. Returns true if the action is possible.
-   * The character can only pickup an item if it is
-   * 1. on the same tile
-   * 2. the current weight of all items the character carries + the weight of the item is less then maxWeight
-   *
-   * @param what the item to be picked up
-   * @return  boolean <code>true</code> if the action was successful, <code>false</code> otherwise
-   */
-  public boolean pickUp(Wearable what) {
-    // TODO Auto-generated method stub: CHECK
-    Tile CharLoc = this.tile;
-    Tile ItemLoc = what.getTile();
-    
-    Double sum = 0.0;
-    for (Wearable item : items) {
-      sum = Double.sum(sum, item.weight);
-    }
-    if (this.maxWeight > sum && CharLoc == ItemLoc) {
-      ItemLoc.onTile().remove(what);
-      this.items.add(what);
-      return true;
-    }
-    else {
-      return false;
+    public Character() {
 
     }
-  }
 
-  /**
-   * @return void
-   */
-  public void interact() {
-    // TODO Auto-generated method stub
-  }
+    /**
+     * Change my position to the given Tile.
+     *
+     * @param destination
+     * @return void
+     */
+    public void move(Tile destination) {
+        // TODO: FILL THIS: KTN, check it!
 
-  public Item activeWeapon() {
-    return activeWeapon;
-  }
+        this.tile = destination;
+    }
 
-  public Tile getTile() {
-    return tile;
-  }
+    /**
+     * Pick up the given Wearable. Returns true if the action is possible. The
+     * character can only pickup an item if it is 1. on the same tile 2. the
+     * current weight of all items the character carries + the weight of the
+     * item is less then maxWeight
+     *
+     * @param what the item to be picked up
+     * @return boolean <code>true</code> if the action was successful,
+     * <code>false</code> otherwise
+     */
+    public boolean pickUp(Wearable what) {
+        // TODO Auto-generated method stub: CHECK
+        Tile CharLoc = this.tile;
+        Tile ItemLoc = what.getTile();
 
-  public int hasForce() {
-    return power;
-  }
+        Double sum = 0.0;
+        for (Wearable item : items) {
+            sum = Double.sum(sum, item.weight);
+        }
+        if (this.maxWeight > sum && CharLoc == ItemLoc) {
+            ItemLoc.onTile().remove(what);
+            this.items.add(what);
+            return true;
+        } else {
+            return false;
 
-  public int levelDown() {
-    return ++level;
-  }
+        }
+    }
 
-  public int levelUp() {
-    return --level;
-  }
+    /**
+     * @return void
+     */
+    public void interact() {
+        // TODO Auto-generated method stub
+    }
 
-  /**
-   * Apply the effects of an attack, taking into account the armor
-   */
-  public void applyAttack(CharacterModifier eff) {
-    /*
+    public Item activeWeapon() {
+        return activeWeapon;
+    }
+
+    public Tile getTile() {
+        return tile;
+    }
+
+    public int hasForce() {
+        return power;
+    }
+
+    public int levelDown() {
+        return ++level;
+    }
+
+    public int levelUp() {
+        return --level;
+    }
+
+    /**
+     * Apply the effects of an attack, taking into account the armor
+     */
+    public void applyAttack(CharacterModifier eff) {
+        /*
      * Example of an attack - an adversary uses his weapon (different dimensions,
      * like affecting health, armor, magic ability, and how long the effect
      * persists)
@@ -173,47 +173,67 @@ public class Character implements Storable, TraitedTileOccupier {
      * - several factors modulate the outcome of this effect: current health
      * stamina, quality of different armors, possibly even in the different
      * dimensions.
+         */
+
+    }
+
+    /**
+     * Apply the effects of, e.g., a poisoning, eating something, etc.
      */
+    public void applyItem(CharacterModifier eff) {
 
-  }
+        // turns is for how many turns will the effects occur
+        int turns = eff.howLong();
 
-  /**
-   * Apply the effects of, e.g., a poisoning, eating something, etc.
-   */
-  public void applyItem(CharacterModifier eff) {
-  }
+        while (turns > 0) {
+            // implement effects: health, magic, power
+            Character affected = this;
+            affected.health += eff.health;
+            affected.magic += eff.magic;
+            affected.power += eff.power;
 
-  /**
-   * removes the given Item from the characters inventory
-   * @param item the item to be removed
-   * @return <code>true</code> if the action was successful, <code>false</code> otherwise
-   */
-  public boolean dropItem(Item item){
-    // TODO please implement me!
-    return false;
-  }
+            turns -= 1;
+        }
 
-  /**
-   * Equips the given Wearable as active Weapon or armor depending
-   * @param wearable the item to be equipped
-   * @return <code>true</code> the action was successful, <code>false</code> otherwise
-   */
-  public boolean equipItem(Wearable wearable){
-    // TODO please implement me!
-    return false;
-  }
+    }
 
-  @Override
-  public String getTrait() { return (health == 0 ? "DEAD_" : "") + role; }
+    /**
+     * removes the given Item from the characters inventory
+     *
+     * @param item the item to be removed
+     * @return <code>true</code> if the action was successful,
+     * <code>false</code> otherwise
+     */
+    public boolean dropItem(Item item) {
+        // TODO please implement me!
+        return false;
+    }
 
-  @Override
-  public void marshal(MarshallingContext c) {
-    // TODO fill this
-  }
+    /**
+     * Equips the given Wearable as active Weapon or armor depending
+     *
+     * @param wearable the item to be equipped
+     * @return <code>true</code> the action was successful, <code>false</code>
+     * otherwise
+     */
+    public boolean equipItem(Wearable wearable) {
+        // TODO please implement me!
+        return false;
+    }
 
-  @Override
-  public void unmarshal(MarshallingContext c) {
-    // TODO fill this
-  }
+    @Override
+    public String getTrait() {
+        return (health == 0 ? "DEAD_" : "") + role;
+    }
+
+    @Override
+    public void marshal(MarshallingContext c) {
+        // TODO fill this
+    }
+
+    @Override
+    public void unmarshal(MarshallingContext c) {
+        // TODO fill this
+    }
 
 }
