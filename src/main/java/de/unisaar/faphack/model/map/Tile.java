b6f90cfaed70fb813ec00ffad7ccfa5513786e28
Lena Meth <eleni.metheniti@dfki.de>
2019-07-25 13:59:56 +0200

    marshal Tile

--- src/main/java/de/unisaar/faphack/model/map/Tile.java
+++ src/main/java/de/unisaar/faphack/model/map/Tile.java
@@ -12,98 +12,107 @@ import java.util.List;
  */
 public abstract class Tile implements Storable, TraitOwner {
 
-  protected int x;
-  protected int y;
+    protected int x;
+    protected int y;
 
-  /**
-   * The room this tile is located in. This must not be null.
-   */
-  protected Room room;
+    /**
+     * The room this tile is located in. This must not be null.
+     */
+    protected Room room;
 
-  /**
-   * The trait of this item.
-   */
-  protected String trait;
+    /**
+     * The trait of this item.
+     */
+    protected String trait;
 
-  public Tile() {
+    public Tile() {
 
-  }
+    }
 
-  public Tile(int x, int y, Room room){
-    this.room = room;
-    this.x = x;
-    this.y = y;
-  }
+    public Tile(int x, int y, Room room) {
+        this.room = room;
+        this.x = x;
+        this.y = y;
+    }
 
-  public int getX() {
-    return x;
-  }
+    public int getX() {
+        return x;
+    }
 
-  public int getY() {
-    return y;
-  }
+    public int getY() {
+        return y;
+    }
 
-  //public Room getRoom() {
-   // return room;
-  //}
+    //public Room getRoom() {
+    // return room;
+    //}
+    /**
+     * Given the "vector" d, what's the tile you get in return? (Hint: ask the
+     * room)
+     *
+     * @return the next tile in direction d
+     */
+    public Tile getNextTile(Direction d) {
 
-  /**
-   * Given the "vector" d, what's the tile you get in return? (Hint: ask the room)
-   *
-   * @return the next tile in direction d
-   */
-  public Tile getNextTile(Direction d) {
-      
-      return room.getNextTile(this, d);
-  }
+        return room.getNextTile(this, d);
+    }
 
-  /**
-   * Can c proceed onto this tile?
-   *
-   * @return the current tile if you can move the Character c onto this tile, null
-   *         otherwise
-   */
-  public abstract Tile willTake(Character c);
+    /**
+     * Can c proceed onto this tile?
+     *
+     * @return the current tile if you can move the Character c onto this tile,
+     * null otherwise
+     */
+    public abstract Tile willTake(Character c);
 
-  /**
-   *  Almost all tiles can not have items on them.
-   */
-  public List<Item> onTile() {
-    return Collections.emptyList();
-  }
+    /**
+     * Almost all tiles can not have items on them.
+     */
+    public List<Item> onTile() {
+        return Collections.emptyList();
+    }
 
-  @Override
-  public String getTrait() { return trait; }
+    @Override
+    public String getTrait() {
+        return trait;
+    }
 
-  /**
-   * Most tiles have no trap
-   */
-  public Trap hasTrap() {
-    return null;
-  }
+    /**
+     * Most tiles have no trap
+     */
+    public Trap hasTrap() {
+        return null;
+    }
 
-  public void marshal(MarshallingContext c) {
-    // TODO please implement me!
-  }
+    public void marshal(MarshallingContext c) {
+        c.write("room", this.room);
+        c.write("trait", this.trait);
+        c.write("x", this.x);
+        c.write("y", this.y);
 
-  public void unmarshal(MarshallingContext c) {
-    // TODO please implement me!
-  }
+    }
 
-  public boolean removeItem(Wearable what) {
-    return false;
-  }
+    public void unmarshal(MarshallingContext c) {
+        room = c.read("room");
+        trait = c.read("trait");
+        x = c.read("x");
+        y = c.read("y");
+    }
 
-  public boolean addItem(Wearable what){
-    return false;
-  }
+    public boolean removeItem(Wearable what) {
+        return false;
+    }
 
-  /**
-   *  Almost all tiles can not be occupied by a character.
-   */
-  public boolean isOccupied(){
-    return false;
-  }
+    public boolean addItem(Wearable what) {
+        return false;
+    }
+
+    /**
+     * Almost all tiles can not be occupied by a character.
+     */
+    public boolean isOccupied() {
+        return false;
+    }
 
     public Room getRoom() {
         return room;
