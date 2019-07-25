26b703db9afe3872587e345eea8cc78a6fcff68a
Katherine Dunfield <katherine.dunfield@dfki.de>
2019-07-25 14:07:01 +0200

    more json done

--- src/main/java/de/unisaar/faphack/model/JsonMarshallingContext.java
+++ src/main/java/de/unisaar/faphack/model/JsonMarshallingContext.java
@@ -14,199 +14,212 @@ import java.util.logging.Logger;
 
 public class JsonMarshallingContext implements MarshallingContext {
 
-  private final File file;
+    private final File file;
 
-  private static StorableFactory factory;
+    private static StorableFactory factory;
 
-  private IdentityHashMap<Storable, String> writecache;
+    private IdentityHashMap<Storable, String> writecache;
 
-  private Deque<JSONObject> stack;
+    private Deque<JSONObject> stack;
 
-  private Map<String, Storable> readcache;
+    private Map<String, Storable> readcache;
 
-  private int idGenerator = 1;
+    private int idGenerator = 1;
 
-  public JsonMarshallingContext(File f, StorableFactory fact) {
-    file = f;
-    stack = new ArrayDeque<>();
-    factory = fact;
-    writecache = new IdentityHashMap<Storable, String>();
-    readcache = new HashMap<String, Storable>();
- 
-  }
-  
-  private String getIdClassName (Storable s){
-      
-      String idName = s.getClass().getSimpleName() + "@" + String.format("%06d", idGenerator);
-      idGenerator++;
-      
-      return idName;
-  }
-  
-  private JSONObject toJson(Storable s){
-      
-      JSONObject obj = new JSONObject();
-      
-      String idName = getIdClassName(s);
-      
-      //CHECK IF IN WRITECACHE
-      this.writecache.put(s, idName);
-      obj.put("id", idName);
-
-      stack.push(obj);
-      s.marshal(this);
-      stack.pop();
-      
-      return obj;
-  } 
-  
-
-
-  @Override
-  public void save(Storable s) {
-
-   JSONObject obj = new JSONObject();
-   
-    obj = toJson(s);
-    
-    
-    try (FileWriter writer = new FileWriter(this.file)) {
-			writer.write(obj.toJSONString());
-		} catch (IOException ex) {
-          Logger.getLogger(JsonMarshallingContext.class.getName()).log(Level.SEVERE, null, ex);
-      }
-    
-  }
-
-  @Override
-    public Storable read() {  
-        
-    return null;
+    public JsonMarshallingContext(File f, StorableFactory fact) {
+        file = f;
+        stack = new ArrayDeque<>();
+        factory = fact;
+        writecache = new IdentityHashMap<Storable, String>();
+        readcache = new HashMap<String, Storable>();
 
     }
-    
 
-  @Override
-  public void write(String key, Storable object) {
-    
-       if (stack.size() > 0){
-           
-          JSONObject obj = stack.pop();
+    private String getIdClassName(Storable s) {
 
-          JSONObject obj2 = new JSONObject();
-  
-          obj2 = toJson(object);
-      
-          obj.put(key, obj2);
+        String idName = s.getClass().getSimpleName() + "@" + String.format("%06d", idGenerator);
+        idGenerator++;
 
-          stack.push(obj);
+        return idName;
+    }
 
-      } 
-      
-      else {  
-      }
+    public JSONObject toJson(Storable s) {
 
-  }
+        JSONObject obj = new JSONObject();
 
-  @Override
-  public <T extends Storable> T read(String key) {
-    // TODO Auto-generated method stub
-    return null;
-  }
+        String idName = getIdClassName(s);
 
-  @Override
-  public void write(String key, int object) {
-       if (stack.size() > 0){
-          JSONObject obj = stack.pop();
-          obj.put(key, object);
-          stack.push(obj);
-      } 
-      
-      else {  
-      }
+        //CHECK IF IN WRITECACHE
+        this.writecache.put(s, idName);
+        obj.put("id", idName);
 
-  }
+        stack.push(obj);
+        s.marshal(this);
+        stack.pop();
 
-  @Override
-  public int readInt(String key) {
-    // TODO Auto-generated method stub
-    return 0;
-  }
+        return obj;
+    }
 
-  @Override
-  public void write(String key, double object) {
-      if (stack.size() > 0){
-          JSONObject obj = stack.pop();
-          obj.put(key, object);
-          stack.push(obj);
-      } 
-      
-      else {  
-      }
+    @Override
+    public void save(Storable s) {
 
-  }
+        JSONObject obj = new JSONObject();
+        
 
-  @Override
-  public double readDouble(String key) {
-    // TODO Auto-generated method stub
-    return 0;
-  }
+        obj = toJson(s);
 
-  @Override
-  public void write(String key, String object) {
-     if (stack.size() > 0){
-          JSONObject obj = stack.pop();
-          obj.put(key, object);
-          stack.push(obj);
-      } 
-      
-      else {  
-      }
+        try (FileWriter writer = new FileWriter(this.file)) {
+            writer.write(obj.toJSONString());
+        } catch (IOException ex) {
+            Logger.getLogger(JsonMarshallingContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
-  }
+    }
 
-  @Override
-  public String readString(String key) {
-    // TODO Auto-generated method stub
-    return null;
-  }
+    @Override
+    public Storable read() {
 
-  @Override
-  public void write(String key, Collection<? extends Storable> coll) {
-     if (stack.size() > 0){
-          JSONObject obj = stack.pop();
-          obj.put(key, coll);
-          stack.push(obj);
-      } 
-      
-      else {  
-      }
+        return null;
 
-  }
+    }
 
-  @Override
-  public void readAll(String key, Collection<? extends Storable> coll) {
-    // TODO Auto-generated method stub
+    @Override
+    public void write(String key, Storable object) {
 
-  }
 
-  @Override
-  public void write(String key, Tile[][] coll) {
-     if (stack.size() > 0){
-          JSONObject obj = stack.pop();
-          obj.put(key, coll);
-          stack.push(obj);
-      } 
-      
-      else {  
-      }
+        if (stack.size() > 0) {
 
-  }
+            JSONObject obj = stack.pop();
+            JSONObject obj2 = new JSONObject();
+            obj2 = toJson(object);
+            stack.pop();
+            obj.put(key, obj2);
+            stack.push(obj);
 
-  @Override
-  public Tile[][] readBoard(String key) {
-    // TODO Auto-generated method stub
-    return null;
-  }
+        } else {
+        }
+
+    }
+
+    @Override
+    public <T extends Storable> T read(String key) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void write(String key, int object) {
+        
+
+        
+        if (stack.size() > 0) {
+
+            JSONObject obj = stack.pop();
+            obj.put(key, object);
+            stack.push(obj);
+
+        } else {
+        }
+       
+
+    }
+
+    @Override
+    public int readInt(String key) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void write(String key, double object) {
+        
+
+        
+        if (stack.size() > 0) {
+            JSONObject obj = stack.pop();
+            obj.put(key, object);
+            stack.push(obj);
+        } else {
+        }
+        
+
+
+    }
+
+    @Override
+    public double readDouble(String key) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void write(String key, String object) {
+        
+
+        
+        if (stack.size() > 0) {
+            JSONObject obj = stack.pop();
+            obj.put(key, object);
+            stack.push(obj);
+        } else {
+        }
+       
+
+    }
+
+    @Override
+    public String readString(String key) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void write(String key, Collection<? extends Storable> coll) {
+        
+
+
+        if (stack.size() > 0) {
+            JSONObject obj = stack.pop();
+
+            JSONArray arr = new JSONArray();
+
+            for (Storable item : coll) {
+                JSONObject obj2 = toJson(item);
+                arr.add(obj2);
+            }
+
+            obj.put(key, arr);
+            this.stack.push(obj);
+        } else {
+        }
+
+    }
+
+    @Override
+    public void readAll(String key, Collection<? extends Storable> coll) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void write(String key, Tile[][] coll) {
+        
+
+
+        if (stack.size() > 0) {
+            JSONObject obj = stack.pop();
+
+            obj.put(key, coll);
+            stack.push(obj);
+        } else {
+        }
+
+    }
+
+    @Override
+    public Tile[][] readBoard(String key) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
